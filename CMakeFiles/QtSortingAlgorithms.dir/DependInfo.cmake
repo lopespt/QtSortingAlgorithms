b7994587fd268869559fff9bd89dbb7ae97d1c4d
Guilherme Wachs <lopespt@users.noreply.github.com>
2014-09-29 00:33:05 -0300

    Change to CMake

---
+++ CMakeFiles/QtSortingAlgorithms.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/tmp/QtSortingAlgorithms/BubbleSorter.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/BubbleSorter.cpp.o"
+  "/tmp/QtSortingAlgorithms/InsertionSorter.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/InsertionSorter.cpp.o"
+  "/tmp/QtSortingAlgorithms/MergeSorter.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/MergeSorter.cpp.o"
+  "/tmp/QtSortingAlgorithms/QtSortingAlgorithms_automoc.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/QtSortingAlgorithms_automoc.cpp.o"
+  "/tmp/QtSortingAlgorithms/QuickSorter.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/QuickSorter.cpp.o"
+  "/tmp/QtSortingAlgorithms/SortViewWidget.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/SortViewWidget.cpp.o"
+  "/tmp/QtSortingAlgorithms/TimerWidget.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/TimerWidget.cpp.o"
+  "/tmp/QtSortingAlgorithms/VectorSorter.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/VectorSorter.cpp.o"
+  "/tmp/QtSortingAlgorithms/main.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/main.cpp.o"
+  "/tmp/QtSortingAlgorithms/mainWindow.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/mainWindow.cpp.o"
+  "/tmp/QtSortingAlgorithms/qrc_resources.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/qrc_resources.cpp.o"
+  "/tmp/QtSortingAlgorithms/vectoreditor.cpp" "/tmp/QtSortingAlgorithms/CMakeFiles/QtSortingAlgorithms.dir/vectoreditor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "/opt/local/Library/Frameworks/QtWidgets.framework"
+  "/opt/local/Library/Frameworks/QtWidgets.framework/Headers"
+  "/opt/local/Library/Frameworks/QtGui.framework"
+  "/opt/local/Library/Frameworks/QtGui.framework/Headers"
+  "/System/Library/Frameworks/OpenGL.framework/Headers"
+  "/opt/local/Library/Frameworks/QtCore.framework"
+  "/opt/local/Library/Frameworks/QtCore.framework/Headers"
+  "/opt/local/share/qt5/mkspecs/macx-clang"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- moc_TimerWidget.cpp
+++ moc_TimerWidget.cpp
@@ -1,18 +1,18 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'TimerWidget.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.0)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.3.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "TimerWidget.h"
+#include "../../private/tmp/QtSortingAlgorithms/TimerWidget.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'TimerWidget.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.3.0. It"
+#error "This file was generated using the moc from 5.3.1. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
